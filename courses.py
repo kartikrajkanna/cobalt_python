a222b6685cfb8bdec28c67c52186a4c21b27fe84
Kartik Saravana Arvind <kartikrajkanna@gmail.com>
2018-06-04 12:15:02 -0400

    Style fixes

--- courses.py
+++ courses.py
@@ -1,7 +1,8 @@
+import requests
+
+
 API_KEY = "756zx4M5ThXZLOi0JszVjzMOapultFEv"
 
-# Module to make requests
-import requests
 
 # Helper function that uses the url, and given parameters to make HTTP request and get back data
 def _get_from_url(request_url, query=-1, limit=-1, skip=-1, sort=-1):
@@ -17,29 +18,34 @@ def _get_from_url(request_url, query=-1, limit=-1, skip=-1, sort=-1):
     r = requests.get(request_url)
     return r.status_code
 
+
 # Get a list of all courses, (default limit 10, max limit 100)
 def get_courses(limit = -1, skip = -1, sort = -1):
     request_url = "https://cobalt.qas.im/api/1.0/courses"
     return _get_from_url(request_url, -1, limit, skip, sort)
+
 
 # Get a course by its id
 def get_course_by_id(id):
     request_url = "https://cobalt.qas.im/api/1.0/courses/" + id
     return _get_from_url(request_url)
 
+
 # Get a list of courses that satisfy the search query
 def get_courses_by_search(query, limit=-1, skip=-1, sort=-1):
     request_url = "https://cobalt.qas.im/api/1.0/courses/search"
     return _get_from_url(request_url, query, limit, skip, sort)
+
 
 # Get a list of courses that satisfy the filtery query
 def get_courses_by_filter(query, limit=-1, skip=-1, sort=-1):
     request_url = "https://cobalt.qas.im/api/1.0/courses/filter"
     return _get_from_url(request_url, query, limit, skip, sort)
 
+
 # Basic Tests
 if __name__ == "__main__":
-    print("Hello World");
+    print("Hello World")
     print(get_courses(limit=100))
     print(get_course_by_id("CSC148H1F20179"))
     print(get_courses_by_search("Calculus"))
